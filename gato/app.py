44b1134fc174a647540a305a0f2f55a05db0b1eb
chxrry619 <aaron2003ippo@gmail.com>
2025-02-10 14:41:36 -0700

    inicio de ahorcado

--- gato/app.py
+++ gato/app.py
@@ -5,13 +5,16 @@ import tablero
 
 def main():
     ''' Función principal '''
+    X = {"G":0,"P":0,"E":0}
+    O = {"G":0,"P":0,"E":0}
+    score = {"X":X,"O":O}
     numeros =[str(i) for i in range(1,10)]
-    dsimbolos = {x:x for x in numeros}
-    g = tablero.juego(dsimbolos)
-    if g is not None:
-        print(f'El ganador es {g}')
-    else:
-        print('Empate')
+    corriendo = True
+    while corriendo:
+        dsimbolos = {x:x for x in numeros}
+        g = tablero.juego(dsimbolos)
+        tablero.actualiza_score(score,g)
+        tablero.despliega_tablero(score)
 
 if __name__ == '__main__':
     main()
---
+++ linea/hangman/funciones.py
@@ -0,0 +1,10 @@
+def carga_archivo_texto(archivo: str) -> list:
+    with open(archivo, 'r', encoding='utf-8') as file:
+        oraciones = file.readlines()
+    return oraciones
+
+
+if __name__ == '__main__':
+    lista = carga_archivo_texto('./plantilla/bad-apple.txt')
+    for elemento in lista:
+        print(elemento)
