fff810901e2073c4271948cf6431d5e32edd046d
chxrry619 <aaron2003men@gmail.com>
2025-03-12 14:52:33 -0700

    nuevo diseño

--- libros_web/app.py
+++ libros_web/app.py
@@ -1,9 +1,13 @@
 ''' Programa principal de Libros Web '''
 <<<<<<< HEAD
+<<<<<<< HEAD
 from flask import Flask, render_template, request
 =======
 from flask import Flask, render_template, request 
 >>>>>>> 3f163d7 (11 de marzo patch)
+=======
+from flask import Flask, render_template, request
+>>>>>>> 949d374 (nuevo diseño)
 import funciones as fn
 
 app = Flask(__name__)
@@ -15,6 +19,7 @@ diccionario_titulos = fn.crea_diccionario(lista_libros,'title')
 diccionario_autores = fn.crea_diccionario(lista_libros,'author')
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 @app.route('/')
 def inicio():
@@ -23,6 +28,9 @@ def inicio():
 
 @app.route('/titulos', methods =['GET','POST'])
 =======
+=======
+
+>>>>>>> 949d374 (nuevo diseño)
 @app.route('/')
 def inicio():
     ''' Página de inicio de la aplicación '''
@@ -74,15 +82,39 @@ def title():
 =======
     return render_template('titulo.html', lista_libros=resultado)
 
-@app.route('/libro/<id>', methods =['GET'])
-def libro(id:str):
+@app.route('/libro/<id_libro>', methods =['GET'])
+def libro(id_libro:str):
     ''' Página de información de un libro '''
-    if id in diccionario_id:
-        libro = diccionario_id[id]
-        return render_template('libro.html', libro=libro)
+    if id_libro in diccionario_id:
+        book = diccionario_id[id_libro]
+        return render_template('libro.html', libro=book)
     else:
         return render_template('libro.html', libro=None)
     
+<<<<<<< HEAD
 >>>>>>> 3f163d7 (11 de marzo patch)
+=======
+@app.route('/letra/', methods =['GET'])
+def plantilla_letra():
+    ''' Página de búsqueda por letra '''
+    return render_template('letra.html', lista_libros=[])
+
+@app.route('/letra/<letra>', methods =['GET'])
+def busqueda_letra(letra:str):
+    ''' Página de búsqueda por letra '''
+    resultado = fn.libros_empiezan_con(lista_libros, letra)
+    return render_template('letra.html', lista_libros=resultado)
+
+@app.route('/title',  methods =['GET','POST'])
+def title():
+    ''' Página de búsqueda por título '''
+    print(request.method)
+    resultado = []
+    if request.method == 'POST':
+        titulo = request.form.get('searchInput', '')
+        resultado = fn.busca_en_diccionario(diccionario_titulos, titulo)
+    return render_template('titulos.html', lista_libros=resultado)
+
+>>>>>>> 949d374 (nuevo diseño)
 if __name__ == '__main__':
     app.run(debug=True)
