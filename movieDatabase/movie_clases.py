2424709a33be5befebf3a036b4bb8962958bf904
chxrry619 <aaron2003men@gmail.com>
2025-03-19 14:47:58 -0700

    movie_clases actualizacion

--- movieDatabase/movie_clases.py
+++ movieDatabase/movie_clases.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class Actor:
     ''' Clase Actor'''
     def __init__(self,id_estrella,nombre,fecha_nacimiento,ciudad_nacimiento,url_imagen,username):
-        self.id_estrella       = id_estrella
+        self.id_estrella       = int (id_estrella)
         self.nombre            = nombre
         self.fecha_nacimiento  = datetime.strptime(fecha_nacimiento, '%Y-%m-%d')
         self.ciudad_nacimiento = ciudad_nacimiento
@@ -24,11 +24,14 @@ class Actor:
             'username': self.username
         }
     
+    def __str__(self):
+        return self.nombre
+    
 class Pelicula:
     ''' Clase Película'''
     def __init__(self, id_pelicula,titulo_pelicula,fecha_lanzamiento,url_poster):
         ''' Constructor de la clase Película'''
-        self.id_pelicula       = id_pelicula
+        self.id_pelicula       = int(id_pelicula)
         self.titulo_pelicula   = titulo_pelicula
         self.fecha_lanzamiento = datetime.strptime(fecha_lanzamiento, '%Y-%m-%d')
         self.url_poster        = url_poster
@@ -42,13 +45,16 @@ class Pelicula:
             'url_poster': self.url_poster
         }
     
+    def __str__(self):
+        return f'{self.id_pelicula} - {self.titulo_pelicula}'
+
 class Relacion:
     ''' Clase Relación: Relación entre actores y películas'''
     def __init__(self,id_relacion,id_pelicula,id_estrella):
         ''' Constructor de la clase Relación'''
-        self.id_relacion = id_relacion
-        self.id_pelicula = id_pelicula  
-        self.id_estrella = id_estrella
+        self.id_relacion = int(id_relacion)
+        self.id_pelicula = int(id_pelicula)  
+        self.id_estrella = int(id_estrella)
     
     def to_dict(self):
         ''' Retorna un diccionario con los atributos del objeto Relación'''
@@ -89,6 +95,12 @@ class SistemaCine:
         self.relaciones = []
         self.usuarios = []
         self.usuario_actual = None
+        self.idx_actor = 0
+        self.idx_pelicula = 0
+        self.idx_relacion = 0
+        self.idx_user = 0
+
+
     
     def cargar_csv(self, archivo, clase):
         ''' Método para cargar datos desde un archivo CSV'''
@@ -104,6 +116,22 @@ class SistemaCine:
                 elif clase == User:
                     self.usuarios.append(User(**row))
 
+        if clase == Actor:  
+            actor = Actor(**row)
+            self.actores[self.idx_actor] = actor
+        elif clase == Pelicula:
+            pelicula = Pelicula(**row)
+            self.peliculas[self.idx_pelicula] = pelicula
+            elif clase == Relacion:
+                self.relaciones[self.idx_relacion] = Relacion(**row)
+            elif clase == User:
+            user = User(**row)
+            self.usuarios [user.username] = user
+            
+
+def obtener_peliculas_por_actor(self, id_estrella):
+    ids_peliculas = [relacion.id_pelicula for relacion in self.relaciones if relacion.id_estrella == id_estrella]
+    return [pelicula for pelicula in self.peliculas if pelicula.id_pelicula in ids_peliculas]
 if __name__ == '__main__':
     sistema = SistemaCine()
     sistema.cargar_csv('datos/movies_db - actores.csv', Actor)
