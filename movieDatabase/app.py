c23d7897928e0578b299c939b5d9ae49301361f5
chxrry619 <aaron2003men@gmail.com>
2025-04-01 14:44:55 -0700

    cookies y login actualizados

--- movieDatabase/app.py
+++ movieDatabase/app.py
@@ -20,5 +20,31 @@ sistema.cargar_csv(users_csv,mc.User)
 def index():
     return render_template('index.html')
 
+@app.route('/actores')
+def actores():
+    ''' Muestra la lista de actores '''
+    lista_actores = sistema.actores.values()
+    return render_template('actores.html', actores=lista_actores)
+
+@app.route('/peliculas')
+def peliculas():
+    ''' Muestra la lista de peliculas '''
+    lista_peliculas = sistema.peliculas.values()
+    return render_template('peliculas.html', peliculas=lista_peliculas)
+
+@app.route('/actor/<int:id_actor>')
+def actor(id_actor):
+    ''' Muestra la información de un actor '''
+    actor = sistema.actores[id_actor]
+    personajes = sistema.obtener_personajes_por_estrella(id_actor)
+    return render_template('actor.html', actor=actor,lista_peliculas=personajes)
+
+@app.route('/pelicula/<int:id_pelicula>')
+def pelicula(id_pelicula):
+    ''' Muestra la información de una pelicula '''
+    pelicula = sistema.peliculas[id_pelicula]
+    actores = sistema.obtener_personajes_por_pelicula(id_pelicula)
+    return render_template('pelicula.html', pelicula=pelicula, lista_actores=actores)
+
 if __name__ == '__main__':
     app.run(debug=True)
